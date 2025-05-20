6831ce83105361d430b0d9d3b23ddf82c9431a25
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2025-05-20 10:28:51 -0500

    Add files via upload

---
+++ Game/Game-Manager/Player/Player.java
@@ -0,0 +1,45 @@
+// Author: Tyerone Chen
+// Create Date: 5/20/2025
+// Update Date: 5/20/2025
+
+// Player class which handles and stores information such as battle ships, as well as actions
+
+public class Player {
+   // Constants
+   
+   // Variables
+   // game_manager - GameManager Class
+   // fleet - Array / ArrayList of Ships Class - Length 3?
+   //
+   
+   // Overridden Functions
+   // toString -> void - should print out relevant debug info
+   
+   @Override
+   public void toString() {
+      ;
+   }   
+   
+   // Functions
+   // sendHit -> void
+   // dragShip / PlaceShip -> void?
+   // rotateShip -> void - calls ship function to rotate ship
+   // ready -> boolean - Sends a signal to declare this player is ready
+   
+   public void sendHit() {
+      ;
+   }
+   
+   public void dragShip() {
+      ;
+   }
+   
+   public void rotateShip() {
+      ;
+   }
+   
+   public void ready() {
+      ;
+   }
+
+}
